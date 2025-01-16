7b7aeb0ff5f0637eb56396dc0b7821aa476dd20d
Sergey Leontiev <translatertrans@gmail.com>
2025-01-16 16:27:33 +0100

    Remove NYTPhotoViewerGIF

--- Package.swift
+++ Package.swift
@@ -9,24 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "NYTPhotoViewer",
-            targets: ["NYTPhotoViewer", "NYTPhotoViewerGIF"]
+            targets: ["NYTPhotoViewer"]
             ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.1")
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "NYTPhotoViewer",
             path: "NYTPhotoViewer"
-        ),
-        .target(
-            name: "NYTPhotoViewerGIF",
-            dependencies: ["PINRemoteImage"],
-            path: "SourceSymLink",
-            cSettings: [
-              .define("ANIMATED_GIF_SUPPORT", to: "1")
-            ]
         )
     ]
 )
